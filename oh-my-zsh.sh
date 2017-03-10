924d062160baf9c5dd3f00bc3c398fa7236cd7fd
Orestes Carracedo <orestes@orestes.io>
2017-03-10 19:00:45 +0100

    Oh My ZSH updates

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,9 +1,23 @@
 # User theme configuration
 
-ZSH_THEME="random"
+ZSH_THEME=sorin
 DEFAULT_USER=orestes
+DOTFILES=$HOME/.dotfiles
+
+plugins=(git themes httpie zsh-navigation-tools docker yarn)
+killport() { lsof -i tcp:"$@" | awk 'NR!=1 {print $2}' | xargs kill ;}
+
+export LANG=en_US.UTF-8
+export LC_ALL=$LANG
 
 # dev tools
 
-. ~/dotfiles/load-nvm.sh
-. ~/dotfiles/load-rvm.sh # rvm needs to be last, or it'll complain
+source /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
+
+#. $DOTFILES/load-adb.sh
+. $DOTFILES/load-nvm.sh
+. $DOTFILES/load-rvm.sh # rvm needs to be last, or it'll complain
+. $DOTFILES/golang.sh
+
+#export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH="/usr/local/sbin:$PATH"
