ba6c335fda2d8fa794026d8478dbffe2b849c59c
Orestes Carracedo <orestes@orestes.io>
2019-08-10 13:16:23 +0200

    Last update

---
+++ aliases.sh
@@ -0,0 +1,1 @@
+killport() { lsof -i tcp:"$@" | awk 'NR!=1 {print $2}' | xargs kill ;}
--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,23 +1,9 @@
-# User theme configuration
-
-ZSH_THEME=sorin
-DEFAULT_USER=orestes
 DOTFILES=$HOME/.dotfiles
 
-plugins=(git themes httpie zsh-navigation-tools docker yarn)
-killport() { lsof -i tcp:"$@" | awk 'NR!=1 {print $2}' | xargs kill ;}
+plugins=(git zsh-navigation-tools docker yarn themes)
 
-export LANG=en_US.UTF-8
-export LC_ALL=$LANG
-
-# dev tools
-
-source /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
-
-#. $DOTFILES/load-adb.sh
-. $DOTFILES/load-nvm.sh
-. $DOTFILES/load-rvm.sh # rvm needs to be last, or it'll complain
-. $DOTFILES/golang.sh
-
-#export ANDROID_HOME=$HOME/Library/Android/sdk
-export PATH="/usr/local/sbin:$PATH"
+. $DOTFILES/aliases.sh
+. $DOTFILES/nvm.sh
+. $DOTFILES/homebrew.sh
+. $DOTFILES/prompt.sh
+. $DOTFILES/composer.sh
